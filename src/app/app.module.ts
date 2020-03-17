27b984750f793f6e5d616a21c70b6747c9044c0b
rasengaan <39739200+rasengaan@users.noreply.github.com>
2020-03-17 11:22:39 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; 
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HierarichalComponent } from './hierarichal/hierarichal.component';
+import { DepartmentComponent } from './department/department.component';
+import { LocationComponent } from './location/location.component';
+const appRoutes: Routes = [
+  {
+    path: 'hierarichal',
+    component: HierarichalComponent
+  },
+  {
+    path: 'departments',
+    component: DepartmentComponent,
+  },
+  {
+    path: 'location',
+    component: LocationComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/hierarichal',
+    pathMatch: 'full'
+  }//,
+ // { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HierarichalComponent,
+    DepartmentComponent,
+    LocationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    ),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+
+  constructor(private http: HttpClient) { }
+  token = "elZxQlHDSUallvL3OnnH";
+  getData(url: string) {
+    return this.http.get(url,
+      {
+      headers: new HttpHeaders({ 'Authorization': 'Bearer ' + this.token })
+      });
+  }
+
+}
